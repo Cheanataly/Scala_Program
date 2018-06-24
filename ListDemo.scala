661785907deeed96d7a562449af7accb63216f2d
Nataly Chea <34535916+Cheanataly@users.noreply.github.com>
2018-06-24 15:17:58 +0700

    New Program

---
+++ ListDemo.scala
@@ -0,0 +1,10 @@
+object ListDemo
+{
+  def main(args: Array[String])
+  {
+    var fruits= List("banana","apple","mango","star-fruit","dragonfruit");
+   // fruits=fruits + ("strawberry");
+    println(fruits);
+  }
+}
+
